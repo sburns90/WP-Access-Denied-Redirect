5316d457b19bb2444f50acf005452811ec0e9c04
Steve Burns <sburns90@users.noreply.github.com>
2015-07-09 20:54:04 -0400

    Update access-denied-redirect.php

--- access-denied-redirect.php
+++ access-denied-redirect.php
@@ -8,21 +8,6 @@
    Author URI: http://www.StephenBurns.net/
    License: GPL2
 */
-/*  Copyright 2014 Stephen Burns (stephen@StephenBurns.net)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 ?>
 <?php
 function ad_rtl() {
