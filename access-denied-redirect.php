9b77251aa1dfc23a37a3644193c04e4682139c83
Steve Burns <sburns90@users.noreply.github.com>
2016-05-04 10:35:07 -0400

    Update access-denied-redirect.php

--- access-denied-redirect.php
+++ access-denied-redirect.php
@@ -1,14 +1,34 @@
 <?php
-/*
-   Plugin Name: Access Denied Redirect
-   Plugin URI: https://www.github.com/sburns90/WP-Access-Denied-Redirect/
-   Description: Redirect users to the login page when they normally would receive an access denied message. ONLY enable on the PRIMARY site of a multisite install.
-   Version: 1.0
-   Author: Stephen Burns
-   Author URI: http://www.StephenBurns.net/
-   License: GPL2
-*/
+	/*
+	Plugin Name: SB - Access Denied Redirect
+	Plugin URI: http://www.StephenBurns.net
+	Description: Redirect users to the login page when they normally would recieve an access denied message. ONLY intended for use on the main website for the multisite installation.
+	Version: 1.0
+	Author: Stephen Burns
+	Author URI: http://www.StephenBurns.net/
+	License: GPL2
+	*/
+   
+	/*  Copyright 2015 Stephen Burns  (email : Stephen@StephenBurns.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	*/
+   
+	/* TO DO:
+	 * UNFISHIED (4-18-16) Add initial values for the variables and also try to reduce the number of variables used. */
 ?>
+
 <?php
 function ad_rtl() {
 
